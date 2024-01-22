66ba767ab8ea4164f22fa073e388b9bb8832e111
Nur Ahmed <06nurahmed@gmail.com>
2024-01-22 22:04:26 +0600

    production

--- notaboringteam/components/sub/Chat.js
+++ notaboringteam/components/sub/Chat.js
@@ -9,9 +9,7 @@ const Chat = () => {
 
                 <ab-chat
                     chatbotID="ppFCSpmhM9JP_G951InN0"
-                    theme="dark"
-                    orientation="left"
-                    headline="Chat with our AI"
+                    
                     className="px-4 z-[20] fixed"
                 ></ab-chat>
                 <Script
