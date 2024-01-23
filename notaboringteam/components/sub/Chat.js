ec455cad702f04b75cd11fd3672363ed027ac671
Nur Ahmed <06nurahmed@gmail.com>
2024-01-23 13:44:09 +0600

    production

--- notaboringteam/components/sub/Chat.js
+++ notaboringteam/components/sub/Chat.js
@@ -4,7 +4,7 @@ import Script from "next/script";
 const Chat = () => {
     return (
         <>
-            <div className="z-[20] mx-4 fixed">
+            {/* <div className="z-[20] mx-4 fixed">
                 <ab-chat
                     chatbotID="ppFCSpmhM9JP_G951InN0"
                     className="px-4 z-[20] fixed"
@@ -15,13 +15,27 @@ const Chat = () => {
                     initialMessage="How can I help you today?"
                     placeholder="Type your message here..."
                 ></ab-chat>
-                <Script
-                    async
-                    type="text/javascript"
-                    src="https://client.aidbase.ai/chat-widget.ab.js"
-
-                />
+            </div> */}
+            <div class="relative h-32 w-32">
+                <div class="absolute bottom-0 right-0 h-16 w-16">
+                    <ab-chat
+                        chatbotID="ppFCSpmhM9JP_G951InN0"
+                        theme="dark"
+                        orientation="right"
+                        headline="Chat with our AI"
+                        description="Ask any question and our AI will answer!"
+                        initialMessage="How can I help you today?"
+                        placeholder="Type your message here..."
+                    ></ab-chat>
+                </div>
             </div>
+            <Script
+                async
+                type="text/javascript"
+                src="https://client.aidbase.ai/chat-widget.ab.js"
+
+            />
+
         </>
     )
 }
