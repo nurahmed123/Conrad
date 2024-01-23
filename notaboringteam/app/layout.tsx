b388e538bc2e1b0e58f1b83fcd76dff89323973d
Nur Ahmed <06nurahmed@gmail.com>
2024-01-23 17:24:39 +0600

    production

--- notaboringteam/app/layout.tsx
+++ notaboringteam/app/layout.tsx
@@ -25,7 +25,6 @@ export default function RootLayout({
       >
         <StarsCanvas />
         <Navbar />
-        {/* <ResNav/> */}
         {children}
         <Footer />2
       </body>
--- notaboringteam/components/main/Hero.tsx
+++ notaboringteam/components/main/Hero.tsx
@@ -9,7 +9,7 @@ const Hero = () => {
         autoPlay
         muted
         loop
-        className="rotate-180 absolute top-[-340px]  h-full w-full left-0 z-[1] object-cover "
+        className="rotate-180 absolute top-[-340px]  h-[80%] w-full left-0 z-[1] object-cover "
       >
         <source src="/blackhole.webm" type="video/webm" />
       </video>
